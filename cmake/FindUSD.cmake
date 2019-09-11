59a1524bd388f5c03382225d9c82256605ea1b9f
Pal Mezei <sirpalee@gmail.com>
2019-09-11 14:06:58 +1000

    Supporting prefixed USD library names.

--- cmake/FindUSD.cmake
+++ cmake/FindUSD.cmake
@@ -1,12 +1,32 @@
 # Simple module to find USD.
 
+if (LINUX)
+    set(USD_LIB_EXTENSION ".so"
+        CACHE STRING "Extension of USD libraries")
+elseif (WIN32)
+    set(USD_LIB_EXTENSION ".lib"
+        CACHE STRING "Extension of USD libraries")
+else () # MacOS
+    set(USD_LIB_EXTENSION ".dylib"
+        CACHE STRING "Extension of USD libraries")
+endif ()
+
+if (WIN32)
+    set(USD_LIB_PREFIX ""
+        CACHE STRING "Prefix of USD libraries")
+else ()
+    set(USD_LIB_PREFIX lib
+        CACHE STRING "Prefix of USD libraries")
+endif ()
+
 find_path(USD_INCLUDE_DIR pxr/pxr.h
           PATHS ${USD_ROOT}/include
           $ENV{USD_ROOT}/include
           DOC "USD Include directory")
 
+# We need to find either usd or usd_ms, with taking the prefix into account.
 find_path(USD_LIBRARY_DIR
-          NAMES libusd.so libusd.dylib usd.dll
+          NAMES ${USD_LIB_PREFIX}usd${USD_LIB_EXTENSION} ${USD_LIB_PREFIX}usd_ms${USD_LIB_EXTENSION}
           PATHS ${USD_ROOT}/lib
           $ENV{USD_ROOT}/lib
           DOC "USD Libraries directory")
@@ -21,7 +41,7 @@ find_path(USD_MAYA_INCLUDE_DIR usdMaya/api.h
           DOC "USD Maya Include directory")
 
 find_path(USD_MAYA_LIBRARY_DIR
-          NAMES libusdMaya.so libusdMaya.dylib usdMaya.dll
+          NAMES ${USD_LIB_PREFIX}usdMaya${USD_LIB_EXTENSION}
           PATHS ${USD_ROOT}/third_party/maya/lib
           $ENV{USD_ROOT}/third_party/maya/lib
           ${USD_MAYA_ROOT}/third_party/maya/lib
@@ -73,12 +93,48 @@ if(USD_INCLUDE_DIR AND EXISTS "${USD_INCLUDE_DIR}/pxr/pxr.h")
     set(USD_VERSION ${USD_MAJOR_VERSION}.${USD_MINOR_VERSION}.${USD_PATCH_VERSION})
 endif()
 
+set(USD_LIBS ar;arch;cameraUtil;garch;gf;glf;hd;hdSt;hdx;hf;hgi;hgiGL;hio;js;kind;ndr;pcp;plug;pxOsd;sdf;sdr;tf;trace;usd;usdAppUtils;usdGeom;usdHydra;usdImaging;usdImagingGL;usdLux;usdRi;usdShade;usdShaders;usdSkel;usdSkelImaging;usdUI;usdUtils;usdviewq;usdVol;usdVolImaging;vt;work;usd_ms)
+
+foreach (lib ${USD_LIBS})
+    find_library(USD_${lib}_LIBRARY
+        NAMES ${USD_LIB_PREFIX}${lib}${USD_LIB_EXTENSION}
+        HINTS ${USD_LIBRARY_DIR})
+    if (USD_${lib}_LIBRARY)
+        add_library(${lib} INTERFACE IMPORTED)
+        set_target_properties(${lib}
+            PROPERTIES
+            INTERFACE_LINK_LIBRARIES ${USD_${lib}_LIBRARY}
+        )
+        list(APPEND USD_LIBRARIES ${USD_${lib}_LIBRARY})
+    endif ()
+endforeach ()
+
+set(USD_MAYA_LIBS px_vp20;pxrUsdMayaGL;usdMaya)
+
+foreach (lib ${USD_MAYA_LIBS})
+    find_library(USD_MAYA_${lib}_LIBRARY
+        NAMES ${USD_LIB_PREFIX}${lib}${USD_LIB_EXTENSION}
+        HINTS ${USD_MAYA_LIBRARY_DIR})
+    if (USD_MAYA_${lib}_LIBRARY)
+        message(STATUS "Found ${lib}")
+        add_library(${lib} INTERFACE IMPORTED)
+        set_target_properties(${lib}
+            PROPERTIES
+            INTERFACE_LINK_LIBRARIES ${USD_MAYA_${lib}_LIBRARY}
+        )
+        list(APPEND USD_MAYA_LIBRARIES ${USD_MAYA_${lib}_LIBRARY})
+    endif ()
+endforeach ()
+
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(
-    USD
+find_package_handle_standard_args(USD
     REQUIRED_VARS
-    USD_INCLUDE_DIR
-    USD_LIBRARY_DIR
+        USD_INCLUDE_DIR
+        USD_LIBRARY_DIR
+        USD_LIBRARIES
+        USD_MAYA_INCLUDE_DIR
+        USD_MAYA_LIBRARY_DIR
+        USD_MAYA_LIBRARIES
     VERSION_VAR
-    USD_VERSION)
+        USD_VERSION)
